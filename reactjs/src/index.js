d1b3aa488cb0617e94b2def721ddbc59d96dd1d2
Dang Huy <huydang2132@gmail.com>
2023-03-31 22:24:11 +0700

    add fonawesome

---
+++ reactjs/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'react-toastify/dist/ReactToastify.css';
+import App from './containers/App';
+import IntlProviderWrapper from "./hoc/IntlProviderWrapper";
+import './styles/styles.scss';
+
+import { Provider } from 'react-redux';
+import reduxStore, { persistor } from './redux';
+
+const renderApp = () => {
+  ReactDOM.render(
+    <Provider store={reduxStore}>
+      <IntlProviderWrapper>
+        <App persistor={persistor} />
+      </IntlProviderWrapper>
+    </Provider>,
+    document.getElementById('root')
+  );
+};
+
+renderApp();
